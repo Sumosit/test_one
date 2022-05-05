f9ec60f8359dfd9b13a7d5e0dc47c761a381bc90
Yerlan <isaev.yerlan@kase.kz>
2022-05-05 15:08:10 +0600

    start

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/<app-name>'));
+app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
